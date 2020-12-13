a8ff62031263ad036a9bbbfe3c59e16d2814db22
Zinadin Zidan <zidan44@pixelexperience.org>
2020-12-13 17:26:21 +0600

    mtk-ims: Add Makefile To Simply Iinherit All
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I5765d972b7804598bd3f05e786b92dd8e9562b3a

---
+++ mtk-ims.mk
@@ -0,0 +1,13 @@
+# Radio IMS
+PRODUCT_PACKAGES += \
+    libsink \
+    libimsma \
+    libsource \
+    libsignal \
+    libcomutils \
+    libvt_avsync \
+    libimsma_rtp \
+    libvcodec_cap \
+    libimsma_adapt \
+    libvcodec_capenc \
+    libimsma_socketwrapper
