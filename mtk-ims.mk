a9f218ea8c03be898a21bb24cb2a396f5dc9fba4
AgentFabulous <kshitijgm@gmail.com>
2020-12-27 21:38:38 +0600

    mtk-ims: Build Mediatek IMS Boot Jars
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I3edbcf16a4e9b2713ba9d7a280b4f3235c24ac4b

--- mtk-ims.mk
+++ mtk-ims.mk
@@ -25,6 +25,16 @@ PRODUCT_PACKAGES += \
     libmtkperf_client \
     libimsma_socketwrapper
 
+# Radio IMS Boot Jar
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # Radio IMS Jars
 PRODUCT_PACKAGES += \
     mediatek-common \
