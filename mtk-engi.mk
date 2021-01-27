f122bd5c1a459e35267900e88bfc63b8ef848280
Zinadin Zidan <zidan44@pixelexperience.org>
2021-01-27 12:04:47 +0600

    mtk-ims: permissions: Generate privapp-permissions For EngineerMode
    Reference:[0]: https://source.android.com/devices/tech/config/perms-whitelist
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I3b01f2cdc35f9f09e30ffcef7024c21d8a617520

--- mtk-engi.mk
+++ mtk-engi.mk
@@ -2,3 +2,7 @@
 PRODUCT_PACKAGES += \
     EngineerMode \
     libem_support_jni
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-mediatek-engineer.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek-engineer.xml
